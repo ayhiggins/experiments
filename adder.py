3c0720c0abd052221ab5224a8a7ff66e76c06e7f
Bill Higgins <billh@ibm.com>
2020-06-09 08:01:32 -0400

    Guard against bad input (#4)

--- adder.py
+++ adder.py
@@ -4,13 +4,19 @@
 num1 = 2
 num2 = 5
 
-# Ask the user to input an integer; Python will initially store it as text
-print ('Enter an integer:')
-user_input = input()
-
-# Change the type of the user input from text to an actual integer, so that we
-# can add it in the next step
-num3 = int(user_input)
+# Create the 'num3' variable, but initially give it the special 'None' value,
+# since it doesn’t yet have the user’s integer input
+num3 = None
+# Keep running the following block of code until the user submits an integer
+while num3 is None:
+    # Prompt the user; Python will initially store the input as text
+    print ('Enter an integer:')
+    user_input = input()
+    # Try to convert 'user_input' to text, but guard against bad input
+    try:
+        num3 = int(user_input)
+    except ValueError:
+        print (user_input + ' is not an integer.')
 
 # Add the three numbers and store the result in the new variable 'sum'
 sum = num1 + num2 + num3
